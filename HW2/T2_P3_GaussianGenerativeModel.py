58d62df41f2fdcc20a14d0616842b6abca385771
Ben Ray <benray@college.harvard.edu>
2022-02-22 23:58:27 -0500

    T2_P3_GaussianGenerativeModel.py

--- HW2/T2_P3_GaussianGenerativeModel.py
+++ HW2/T2_P3_GaussianGenerativeModel.py
@@ -1,3 +1,4 @@
+from functools import total_ordering
 import numpy as np
 from scipy.stats import multivariate_normal as mvn  # you may find this useful
 
@@ -11,6 +12,17 @@ from scipy.stats import multivariate_normal as mvn  # you may find this useful
 class GaussianGenerativeModel:
     def __init__(self, is_shared_covariance=False):
         self.is_shared_covariance = is_shared_covariance
+        
+        self.mean_matrix = np.zeros((3,2))
+        
+        self.cov_matrix = np.zeros((2,2))
+        self.cov_matrix0 = np.zeros((2,2))
+        self.cov_matrix1 = np.zeros((2,2))
+        self.cov_matrix2 = np.zeros((2,2))
+
+        self.num_class0 = 0
+        self.num_class1 = 0
+        self.num_class2 = 0
 
     # Just to show how to make 'private' methods
     def __dummyPrivateMethod(self, input):
@@ -18,18 +30,77 @@ class GaussianGenerativeModel:
 
     # TODO: Implement this method!
     def fit(self, X, y):
-        return
+        # X is 27 x 2
+        magnitudes = (X.T)[0]
+        temperatures = (X.T)[1]
+
+        self.num_class0 = np.count_nonzero(y == 0)
+        self.num_class1 = np.count_nonzero(y == 1)
+        self.num_class2 = np.count_nonzero(y == 2)
+
+        mag0 = 0
+        temp0 = 0
+        mag1 = 0
+        temp1 = 0
+        mag2 = 0
+        temp2 = 0
+        X_0 = []
+        X_1 = []
+        X_2 = []
+
+        for i, y_n in enumerate(y):
+            if y_n == 0:
+                mag0 += magnitudes[i]
+                temp0 += temperatures[i]
+                X_0.append(X[i])
+            if y_n == 1:
+                mag1 += magnitudes[i]
+                temp1 += temperatures[i]
+                X_1.append(X[i])
+            if y_n == 2:
+                mag2 += magnitudes[i]
+                temp2 += temperatures[i]
+                X_2.append(X[i])
+        
+        av_mag0 = mag0 / self.num_class0
+        av_mag1 = mag1 / self.num_class1
+        av_mag2 = mag2 / self.num_class2
+        av_temp0 = temp0 / self.num_class0
+        av_temp1 = temp1 / self.num_class1
+        av_temp2 = temp2 / self.num_class2
+
+        self.mean_matrix = np.array([[av_mag0, av_temp0], [av_mag1, av_temp1], [av_mag2, av_temp2]]) # 3 x 2
+
+        self.cov_matrix = np.cov(X.T) # 2 x 2
+        self.cov_matrix0 = np.cov(np.array(X_0).T)
+        self.cov_matrix1 = np.cov(np.array(X_1).T)
+        self.cov_matrix2 = np.cov(np.array(X_2).T)
+
+        return 
 
     # TODO: Implement this method!
     def predict(self, X_pred):
-        # The code in this method should be removed and replaced! We included it
-        # just so that the distribution code is runnable and produces a
-        # (currently meaningless) visualization.
-        preds = []
-        for x in X_pred:
-            z = np.sin(x ** 2).sum()
-            preds.append(1 + np.sign(z) * (np.abs(z) > 0.3))
-        return np.array(preds)
+        
+        total = (X_pred.shape)[0]
+        
+        predictions = []
+
+        for row in X_pred:
+            classification_probs = []
+
+            if self.is_shared_covariance:
+                prob_class0 = mvn.pdf(row, self.mean_matrix[0], self.cov_matrix) * self.num_class0 / total
+                prob_class1 = mvn.pdf(row, self.mean_matrix[1], self.cov_matrix) * self.num_class1 / total
+                prob_class2 = mvn.pdf(row, self.mean_matrix[2], self.cov_matrix) * self.num_class2 / total
+            else:
+                prob_class0 = mvn.pdf(row, self.mean_matrix[0], self.cov_matrix0) * self.num_class0 / total
+                prob_class1 = mvn.pdf(row, self.mean_matrix[1], self.cov_matrix1) * self.num_class1 / total
+                prob_class2 = mvn.pdf(row, self.mean_matrix[2], self.cov_matrix2) * self.num_class2 / total
+            classification_probs = [prob_class0, prob_class1, prob_class2]
+            
+            predictions.append(np.argmax(classification_probs))
+        
+        return np.array(predictions)
 
     # TODO: Implement this method!
     def negative_log_likelihood(self, X, y):
